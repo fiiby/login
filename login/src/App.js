8617123cc019fe9f2741c4a789f5d54df2bdf05e
“fathia <toohanour@gmail.com>
2023-11-15 07:21:56 +0300

    adding some styling

--- login/src/App.js
+++ login/src/App.js
@@ -1,7 +1,7 @@
-
+import React from 'react'
 import './index.css';
 
-import login from'./login'
+import login from'../src/Components/login'
 
 function App() {
   return (
--- login/src/Components/login.js
+++ login/src/Components/login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../index.css';
 
+
 export const login = () => {
   return (
     //  <div className = "container">
