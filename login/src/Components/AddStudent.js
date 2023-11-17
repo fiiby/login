b685a048561c2bb336a75a36501fad099031a65c
“fathia <toohanour@gmail.com>
2023-11-17 16:48:06 +0300

    installed toastify,axios,add some code in add student components

--- login/src/Components/AddStudent.js
+++ login/src/Components/AddStudent.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import {useState} from 'react';
+import axios from 'axios';
 import {Student} from 'phosphor-react';
-import 'react-toastify-'
+import 'react-toastify/dist/ReactToastify.css';
+// import {ToastContainer}
+import {toast} from 'react-toastify';
 
 
 
 
 const AddStudent= ()=>{
+  const [data, setData]=useState({
+    firstName:"",
+    lastName:"",
+    gender:""
+  })
+  const handleChange = (e)=>{
+    setData({...data, [e.target.name]: e.target.value})
+  }
+  const saveStudent=(e)=>{
+    e.preventDefault()
+    axios.post('http://localhost:4000/student', data)
+    .then(res=>{
+      toast.success('New Student added successfully', {
+        position:toast.POSITION.TOP_RIGHT, 
+        autoClose:3000,
+      });
+      setData(data);
+    })
+    .catch(err => {
+      toast.error('There is an error with your input', {
+        position:toast.POSITION.TOP_RIGHT, 
+        autoCLose:3000,
+      });
+    })
+  }
 
   return (
-    <form className="container">
+    <form  onSubmit= {saveStudent} className="container">
         <h2 className="header">Add New Student</h2>
         <div className="underline"></div>
       <div className="inputs">
